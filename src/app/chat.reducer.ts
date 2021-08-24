1fe5807c7d0e72e1e5932cb1ff9bcda8b20676a2
nischitha-gudigar <nischithagudigardr@gmail.com>
2021-08-24 08:54:39 +0530

    1 file modified

--- src/app/chat.reducer.ts
+++ src/app/chat.reducer.ts
@@ -4,16 +4,18 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { chatActionState, MyAppState } from './app.state';
 import { addChat } from './chat.action';
 
-export const initialState: chatActionState[] = [
-  {
-    chatId: null,
-    chatParticular: []
-  }
-];
+export const initialState: chatActionState[] = [];
 
 const _chatReducer = createReducer(
   initialState,
   on(addChat, (state: chatActionState[], message) => {
+    if (state.length > 1) {
+      // let result = state.filter(
+      //   o1 => !message.messageData.some(o2 => o1.chatId === o2.chatId)
+      // );
+      // console.log('in ssssssssssssss');
+      // console.log(result);
+    }
     return {
       ...state,
       ...message.messageData
